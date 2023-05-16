49e1062b74512b8ae2f1bad9e80395d3dc63ce08
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2023-05-16 10:07:49 -0400

    cleanup dead code
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- internal/srv/handlers_test.go
+++ internal/srv/handlers_test.go
@@ -14,70 +14,6 @@ import (
 	"go.infratographer.com/loadbalanceroperator/internal/utils"
 )
 
-var (
-//	msg = pubsubx.Message{
-//		SubjectURN: uuid.NewString(),
-//		EventType:  "create",
-//		Source:     "lbapi",
-//		Timestamp:  time.Now(),
-//		ActorURN:   uuid.NewString(),
-//	}
-)
-
-// func (suite *srvTestSuite) TestParseLBData() {
-// 	suite.T().Parallel()
-
-// 	type testCase struct {
-// 		name        string
-// 		data        map[string]interface{}
-// 		expectError bool
-// 	}
-
-// 	testCases := []testCase{
-// 		{
-// 			name:        "valid data",
-// 			expectError: false,
-// 			data: map[string]interface{}{
-// 				"load_balancer_id": uuid.New(),
-// 				"location_id":      uuid.New(),
-// 			},
-// 		},
-// 		{
-// 			name:        "unable to parse event data",
-// 			expectError: true,
-// 			data: map[string]interface{}{
-// 				"load_balancer_id": 1,
-// 				"location_id":      2,
-// 			},
-// 		},
-// 		{
-// 			name:        "unable to load event data",
-// 			expectError: true,
-// 			data: map[string]interface{}{
-// 				"other field": make(chan int),
-// 			},
-// 		},
-// 	}
-
-// 	for _, tcase := range testCases {
-// 		suite.T().Run(tcase.name, func(t *testing.T) {
-// 			lbData := events.LoadBalancerData{}
-// 			srv := &Server{
-// 				Logger: zap.NewNop().Sugar(),
-// 			}
-// 			msg.AdditionalData = tcase.data
-// 			err := srv.parseLBData(&tcase.data, &lbData)
-
-// 			if tcase.expectError {
-// 				assert.NotNil(t, err)
-// 			} else {
-// 				assert.Nil(t, err)
-// 				assert.NotNil(t, lbData)
-// 			}
-// 		})
-// 	}
-// }
-
 func (suite srvTestSuite) TestMessageRouter() { //nolint:govet
 	type testCase struct {
 		name        string
@@ -148,11 +84,6 @@ func (suite srvTestSuite) TestMessageRouter() { //nolint:govet
 				EventType: "create",
 			},
 		},
-		// {
-		// 	name:        "bad message",
-		// 	msg:         "bad message",
-		// 	expectError: false,
-		// },
 	}
 
 	for _, tcase := range testCases {
